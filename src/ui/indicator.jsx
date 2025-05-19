e08d2d3101ce989d9a221608986f68ff4d99ded0
Tiago Bankow <tiago.bankow@gmail.com>
2025-05-19 15:18:36 -0400

    fix: indicator component

--- src/ui/indicator.jsx
+++ src/ui/indicator.jsx
@@ -1,25 +1,45 @@
-import { cn } from "../utils";
 import { Bad, Good, Regular } from "./icons";
 import { Card } from "./card";
 
 const Indicator = ({ score }) => {
+  if (score > 2.5) {
+    return (
+      <Card className={"mb-2 border-green-600 py-3"}>
+        <div className="border-primary inline-flex w-full items-center justify-between border-b text-lg font-bold text-nowrap">
+          <p>INDICADOR DE MANUTENÇÃO DO IMÓVEL:</p>
+
+          <div className="inline-flex gap-2">
+            <p>BOM</p>
+            <Good />
+          </div>
+        </div>
+      </Card>
+    );
+  }
+
+  if (score < 1.5) {
+    return (
+      <Card className={"mb-2 border-red-600 py-3"}>
+        <div className="border-primary inline-flex w-full items-center justify-between border-b text-lg font-bold text-nowrap">
+          <p>INDICADOR DE MANUTENÇÃO DO IMÓVEL:</p>
+
+          <div className="inline-flex gap-2">
+            <p>Ruim</p>
+            <Bad />
+          </div>
+        </div>
+      </Card>
+    );
+  }
+
   return (
-    <Card
-      className={cn(
-        "mb-2 py-3",
-        score > 2.5
-          ? "border-green-600"
-          : score < 1.5
-            ? "border-red-600"
-            : "border-yellow-600",
-      )}
-    >
+    <Card className={"mb-2 border-yellow-600 py-3"}>
       <div className="border-primary inline-flex w-full items-center justify-between border-b text-lg font-bold text-nowrap">
         <p>INDICADOR DE MANUTENÇÃO DO IMÓVEL:</p>
 
         <div className="inline-flex gap-2">
-          <p>{score > 2.5 ? "BOM" : score < 1.5 ? "RUIM" : "REGULAR"}</p>
-          {score > 2.5 ? <Good /> : score < 1.5 ? <Bad /> : <Regular />}
+          <p>REGULAR</p>
+          <Regular />
         </div>
       </div>
     </Card>
